4c79af01b61764d61f119bbf5cbe73988b7f1dc1
NeoMatrix <lanxis2372615@gmail.com>
2016-06-11 14:11:33 +0800

    finished netcat.py

--- netcat.py
+++ netcat.py
@@ -39,6 +39,103 @@ def usage():
     
     sys.exit(0)
 
+def clientSender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        client.connect((target, port))
+        
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recvLen = 1
+            response = ""
+            
+            while recvLen:
+                data = client.recv(4096)
+                recvLen = len(data)
+                response += data
+
+                if recvLen < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input("")
+            buffer += "\n"
+
+            client.send(buffer)
+
+    except:
+        print "[*] Exception! Exiting."
+        client.close()
+
+def runCommand(command):
+    command = command.rstrip()
+    
+    try:
+        output = subprocess.check_output(command, stderr = subprocess, STDOUT, shell = True)
+    except:
+        output = "Failed to execute command.\r\n"
+
+    return output
+
+def clientHandler(clientSocket):
+    global upload
+    global execute
+    global command
+
+    if len(uploadDest):
+        fileBuffer = ""
+        
+        while True:
+            data = clientSocket.recv(1024)
+            
+            if not data:
+                break
+            else:
+                fileBuffer += data
+
+        try:
+            fileDescriptor = open(uploadDest, "wb")
+            fileDescriptor.write(fileBuffer)
+            fileDescriptor.close()
+
+            clientSocket.send("Successfully saved file to %s\r\n" % uploadDest)
+        except:
+            clientSocket.send("Failed to save file to %s\r\n" % uploadDest)
+
+    if len(execute):
+        output = runCommand(execute)
+        clientSocket.send(output)
+
+    if command:
+        while True:
+            clientSocket.send("<BHP:#> ")
+            cmdBuffer = ""
+            while "\n" not in cmdBuffer:
+                cmdBuffer += clientSocket.recv(1024)
+                response = runCommand(cmdBuffer)
+                clientSocket.send(response)
+
+def serverLoop():
+    global target
+    
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        clientSocket, addr = server.accept()
+        
+        clientThread = threading.Thread(target = clientHandler, args = (clientSocket, ))
+        clientThread.start()
+
 def main():
     global listen
     global port
